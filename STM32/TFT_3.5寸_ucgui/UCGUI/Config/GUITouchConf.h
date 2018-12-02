ca8df86e7b70e4ac5c4a892636e34932031d0ffc
fukangwei <15262751239@163.com>
2018-12-02 13:13:35 +0900

    fix the code

--- STM32/TFT_3.5寸_ucgui/UCGUI/Config/GUITouchConf.h
+++ STM32/TFT_3.5寸_ucgui/UCGUI/Config/GUITouchConf.h
@@ -16,16 +16,13 @@ File        : GUITouch.Conf.h
 Purpose     : Configures touch screen module
 ----------------------------------------------------------------------
 */
-
-
 #ifndef GUITOUCH_CONF_H
 #define GUITOUCH_CONF_H
 
-
-#define GUI_TOUCH_AD_LEFT  20   
-#define GUI_TOUCH_AD_RIGHT  240    
-#define GUI_TOUCH_SWAP_XY    1
-#define GUI_TOUCH_MIRROR_X   0
-#define GUI_TOUCH_MIRROR_Y   1
+#define GUI_TOUCH_AD_LEFT  20
+#define GUI_TOUCH_AD_RIGHT 240
+#define GUI_TOUCH_SWAP_XY  1
+#define GUI_TOUCH_MIRROR_X 0
+#define GUI_TOUCH_MIRROR_Y 1
 
 #endif /* GUITOUCH_CONF_H */
