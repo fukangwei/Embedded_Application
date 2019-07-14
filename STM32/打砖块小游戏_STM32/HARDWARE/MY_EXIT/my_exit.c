be16b76b79df80449cf8791a6a6507070a5414fc
fukangwei <15262751239@163.com>
2019-07-14 20:33:59 +0800

    fix the code

---
+++ STM32/打砖块小游戏_STM32/HARDWARE/MY_EXIT/my_exit.c
@@ -0,0 +1,64 @@
+#include "stm32f10x.h"
+#include "led.h"
+#include "sys.h"
+#include "my_exit.h"
+
+volatile int move_left = 0; /* 向左移动标识符 */
+volatile int move_right = 0; /* 向右移动标识符 */
+
+void key_init(void)
+{
+    GPIO_InitTypeDef  GPIO_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+    EXTI_InitTypeDef EXTI_InitStructure;
+    RCC_APB2PeriphClockCmd ( RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE );
+    
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init ( GPIOA, &GPIO_InitStructure );
+    
+    GPIO_EXTILineConfig ( GPIO_PortSourceGPIOA, GPIO_PinSource0 );
+    EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init ( &EXTI_InitStructure );
+
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn; 
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1; 
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;                               
+    NVIC_Init ( &NVIC_InitStructure );
+    
+    GPIO_EXTILineConfig ( GPIO_PortSourceGPIOA, GPIO_PinSource1 );
+    EXTI_InitStructure.EXTI_Line = EXTI_Line1;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init ( &EXTI_InitStructure );
+
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn; 
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2; 
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;                               
+    NVIC_Init ( &NVIC_InitStructure );
+}
+
+void EXTI0_IRQHandler ( void ) {
+	if ( EXTI_GetITStatus ( EXTI_Line0 ) != RESET ) { /* 判断某个线上的中断是否发生 */
+		/* 中断逻辑 */
+		EXTI_ClearITPendingBit ( EXTI_Line0 ); /* 清除LINE上的中断标志位 */
+        LED0 = !LED0;
+        move_left = 1;
+	}
+}
+
+void EXTI1_IRQHandler ( void ) {
+	if ( EXTI_GetITStatus ( EXTI_Line1 ) != RESET ) { /* 判断某个线上的中断是否发生 */
+		/* 中断逻辑 */
+		EXTI_ClearITPendingBit ( EXTI_Line1 ); /* 清除LINE上的中断标志位 */
+        LED1 = !LED1;
+        move_right = 1;
+	}
+}
---
+++ STM32/打砖块小游戏_STM32/USER/main.c
@@ -0,0 +1,162 @@
+#include "led.h"
+#include "delay.h"
+#include "sys.h"
+#include "usart.h"
+#include <math.h>
+#include "lcd.h"
+#include "my_exit.h"
+
+extern volatile int move_left;
+extern volatile int move_right;
+
+void simulate ( void ) {
+	int score_num = 0; /* 分数 */
+	const int r = 20; /* 小球半径 */
+	const int l = 320; /* 屏幕的长度 */
+	double x_ball = 160, y_ball = 330; /* 小球的实时位置 */
+	double vx = 0, vy = 0; /* x、y方向上的速度分量 */
+	/*----------------------------------------------------*/
+	double x_rect = 160, y_rect = 360; /* 设置矩阵块的中心 */
+	const int w_rect = 80; /* 设置矩阵块的宽度 */
+	const int h_rect = 20; /* 设置矩阵块的高度 */
+	const int move_step = 20; /* 设置矩阵块移动的步长 */
+	/*-------------------------------------------------------*/
+	double left = r; /* 边界左侧 */
+	double right = l - r; /* 边界右侧 */
+	double bottom = y_rect - h_rect / 2 - r; /* 边界底部 */
+	double top = r; /* 边界上部 */
+	/*----------------------------------------------------*/
+	LCD_DrawRectangle ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2 ); /* 画出矩阵块 */
+	LCD_Fill ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2, RED );
+	/*----------------------------------------------------*/
+	LCD_ShowString ( 0, 450, 50, 50, 16, "score:" );
+	LCD_ShowNum ( 50, 450, score_num, 1, 16 );
+	vy = sin ( 45 * 3.14 / 180 ) * 5; /* x方向上速度分量 */
+	vx = cos ( 45 * 3.14 / 180 ) * 5; /* y方向上速度分量 */
+
+	while ( 1 ) {
+		x_ball += vx; /* 增加x方向的分量 */
+		y_ball -= vy; /* 增加y方向的分量 */
+
+		while ( ( x_ball < left ) || ( x_ball > right ) || ( y_ball > bottom ) || ( y_ball < top ) ) {
+			if ( x_ball < left ) { /* 超过左侧 */
+				x_ball = 2 * r - x_ball, vx = -vx;
+			}
+
+			if ( x_ball > right ) { /* 超过右侧 */
+				x_ball = 2 * l - 2 * r - x_ball, vx = -vx;
+			}
+
+			if ( ( y_ball > bottom ) && ( ( x_ball > x_rect - w_rect / 2 - r / 1.41 ) && ( x_ball < x_rect + w_rect / 2 + r / 1.41 ) ) ) { /* 小球触到矩阵块 */
+				y_ball = 2 * ( bottom + r ) - 2 * r - y_ball, vy = -vy;
+				score_num++;
+
+				if ( ( score_num >= 0 ) && ( score_num <= 9 ) ) {
+					LCD_ShowNum ( 50, 450, score_num, 1, 16 );
+				}
+				else if ( ( score_num >= 10 ) && ( score_num <= 99 ) ) {
+					LCD_ShowNum ( 50, 450, score_num, 2, 16 );
+				}
+			}
+			else if ( ( y_ball > bottom ) && ( ( x_ball < x_rect - w_rect / 2  - r / 1.41 ) || ( x_ball > x_rect + w_rect / 2 + - r / 1.41 ) ) ) { /* 小球未触到矩阵块 */
+				LCD_Clear ( GREEN );
+                POINT_COLOR = RED;
+                LCD_ShowString ( 0, 450, 50, 50, 16, "score:" );
+                if ( ( score_num >= 0 ) && ( score_num <= 9 ) ) {
+					LCD_ShowNum ( 50, 450, score_num, 1, 16 );
+				}
+				else if ( ( score_num >= 10 ) && ( score_num <= 99 ) ) {
+					LCD_ShowNum ( 50, 450, score_num, 2, 16 );
+				}
+			}
+
+			if ( y_ball < top ) { /* 超过顶部 */
+				y_ball = 2 * r - y_ball, vy = -vy;
+			}
+		}
+
+		Draw_Circle ( x_ball, y_ball, r );
+		delay_ms ( 10 );
+		LCD_Fill ( x_ball - r, y_ball - r, x_ball + r, y_ball + r, WHITE );
+
+		/*-------------------------------------------------------------------------------------------------------------*/
+		if ( move_left == 1 ) { /* 矩阵块向左移动 */
+			move_left = 0; /* 对移动标志进行复位 */
+
+			if ( x_rect > w_rect / 2 ) { /* 如果未移动到左边缘 */
+				LCD_Fill ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2, WHITE );
+				x_rect -= move_step;
+				LCD_Fill ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2, RED );
+			}
+		}
+
+		if ( move_right == 1 ) { /* 矩阵块向右移动 */
+			move_right = 0; /* 对移动标志进行复位 */
+
+			if ( x_rect < l - w_rect / 2 ) { /* 如果未移动到右边缘 */
+				LCD_Fill ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2, WHITE );
+				x_rect += move_step;
+				LCD_Fill ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2, RED );
+			}
+		}
+	}
+
+#if 0
+	vy = sin ( 45 * 3.14 / 180 ) * 5; /* x方向上速度分量 */
+	vx = cos ( 45 * 3.14 / 180 ) * 5; /* y方向上速度分量 */
+
+	while ( 1 ) {
+		x_ball += vx; /* 增加x方向的分量 */
+		y_ball -= vy; /* 增加y方向的分量 */
+
+		while ( ( x_ball < left ) || ( x_ball > right ) || ( y_ball > bottom ) || ( y_ball < top ) ) {
+			if ( x_ball < left ) { /* 超过左侧 */
+				x_ball = 2 * r - x_ball, vx = -vx;
+			}
+
+			if ( x_ball > right ) { /* 超过右侧 */
+				x_ball = 2 * l - 2 * r - x_ball, vx = -vx;
+			}
+
+			if ( y_ball > bottom ) { /* 超过底部 */
+				y_ball = 2 * w - 2 * r - y_ball, vy = -vy;
+			}
+
+			if ( y_ball < top ) { /* 超过顶部 */
+				y_ball = 2 * r - y_ball, vy = -vy;
+			}
+		}
+
+		Draw_Circle ( x_ball, y_ball, r );
+		delay_ms ( 10 );
+		LCD_Fill ( x_ball - r, y_ball - r, x_ball + r, y_ball + r, WHITE );
+	}
+
+#endif
+}
+
+
+/*-------------------------------------------------------*/
+
+#define DISABLE_JTAG do \
+	{ \
+		RCC->APB2ENR |= 0x00000001; /* 开启afio时钟 */ \
+		AFIO->MAPR = ( 0x00FFFFFF & AFIO->MAPR ) | 0x04000000; /* 关闭JTAG */ \
+	}while(0);
+
+int main ( void ) {
+	DISABLE_JTAG; /* 关闭JTAG功能 */
+	SystemInit();
+	delay_init ( 72 );
+	NVIC_Configuration();
+	uart_init ( 9600 );
+	LED_Init();
+	key_init();
+	LCD_Init();
+	LCD_Clear ( WHITE );
+	POINT_COLOR = RED;
+	simulate();
+
+	while ( 1 );
+}
+
