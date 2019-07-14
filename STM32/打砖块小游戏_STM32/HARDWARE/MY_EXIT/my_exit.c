3ccbe573da15658c4911274fa9407cc4c6634ec7
fukangwei <15262751239@163.com>
2019-07-15 05:57:48 +0800

    fix the code

--- STM32/打砖块小游戏_STM32/HARDWARE/MY_EXIT/my_exit.c
+++ STM32/打砖块小游戏_STM32/HARDWARE/MY_EXIT/my_exit.c
@@ -3,11 +3,11 @@
 #include "sys.h"
 #include "my_exit.h"
 
-volatile int move_left = 0; /* 向左移动标识符 */
-volatile int move_right = 0; /* 向右移动标识符 */
+volatile int move_left = 0;
+volatile int move_right = 0;
 
 void key_init ( void ) {
-    GPIO_InitTypeDef  GPIO_InitStructure;
+    GPIO_InitTypeDef GPIO_InitStructure;
     NVIC_InitTypeDef NVIC_InitStructure;
     EXTI_InitTypeDef EXTI_InitStructure;
     RCC_APB2PeriphClockCmd ( RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE );
@@ -45,18 +45,16 @@ void key_init ( void ) {
 }
 
 void EXTI0_IRQHandler ( void ) {
-    if ( EXTI_GetITStatus ( EXTI_Line0 ) != RESET ) { /* 判断某个线上的中断是否发生 */
-        /* 中断逻辑 */
-        EXTI_ClearITPendingBit ( EXTI_Line0 ); /* 清除LINE上的中断标志位 */
+    if ( EXTI_GetITStatus ( EXTI_Line0 ) != RESET ) {
+        EXTI_ClearITPendingBit ( EXTI_Line0 );
         LED0 = !LED0;
         move_left = 1;
     }
 }
 
 void EXTI1_IRQHandler ( void ) {
-    if ( EXTI_GetITStatus ( EXTI_Line1 ) != RESET ) { /* 判断某个线上的中断是否发生 */
-        /* 中断逻辑 */
-        EXTI_ClearITPendingBit ( EXTI_Line1 ); /* 清除LINE上的中断标志位 */
+    if ( EXTI_GetITStatus ( EXTI_Line1 ) != RESET ) {
+        EXTI_ClearITPendingBit ( EXTI_Line1 );
         LED1 = !LED1;
         move_right = 1;
     }
