f22239a5dedd5210bd7c705a0c64411002e20cef
fukangwei <15262751239@163.com>
2019-07-15 20:25:19 +0800

    fix the code

--- STM32/打砖块小游戏_STM32/USER/main.c
+++ STM32/打砖块小游戏_STM32/USER/main.c
@@ -10,44 +10,41 @@ extern volatile int move_left;
 extern volatile int move_right;
 
 void simulate ( void ) {
-    int score_num = 0; /* 分数 */
-    const int r = 20; /* 小球半径 */
-    const int l = 320; /* 屏幕的长度 */
-    double x_ball = 160, y_ball = 330; /* 小球的实时位置 */
-    double vx = 0, vy = 0; /* x、y方向上的速度分量 */
-    /*----------------------------------------------------*/
-    double x_rect = 160, y_rect = 360; /* 设置矩阵块的中心 */
-    const int w_rect = 80; /* 设置矩阵块的宽度 */
-    const int h_rect = 20; /* 设置矩阵块的高度 */
-    const int move_step = 20; /* 设置矩阵块移动的步长 */
-    /*-------------------------------------------------------*/
-    double left = r; /* 边界左侧 */
-    double right = l - r; /* 边界右侧 */
-    double bottom = y_rect - h_rect / 2 - r; /* 边界底部 */
-    double top = r; /* 边界上部 */
-    /*----------------------------------------------------*/
-    LCD_DrawRectangle ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2 ); /* 画出矩阵块 */
+    int score_num = 0;
+    const int r = 20;
+    const int l = 320;
+    double x_ball = 160, y_ball = 330;
+    double vx = 0, vy = 0;
+    double x_rect = 160, y_rect = 360;
+    const int w_rect = 80;
+    const int h_rect = 20;
+    const int move_step = 20;
+    double left = r;
+    double right = l - r;
+    double bottom = y_rect - h_rect / 2 - r;
+    double top = r;
+
+    LCD_DrawRectangle ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2 );
     LCD_Fill ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2, RED );
-    /*----------------------------------------------------*/
     LCD_ShowString ( 0, 450, 50, 50, 16, "score:" );
     LCD_ShowNum ( 50, 450, score_num, 1, 16 );
-    vy = sin ( 45 * 3.14 / 180 ) * 5; /* x方向上速度分量 */
-    vx = cos ( 45 * 3.14 / 180 ) * 5; /* y方向上速度分量 */
+    vy = sin ( 45 * 3.14 / 180 ) * 5;
+    vx = cos ( 45 * 3.14 / 180 ) * 5;
 
     while ( 1 ) {
-        x_ball += vx; /* 增加x方向的分量 */
-        y_ball -= vy; /* 增加y方向的分量 */
+        x_ball += vx;
+        y_ball -= vy;
 
         while ( ( x_ball < left ) || ( x_ball > right ) || ( y_ball > bottom ) || ( y_ball < top ) ) {
-            if ( x_ball < left ) { /* 超过左侧 */
+            if ( x_ball < left ) {
                 x_ball = 2 * r - x_ball, vx = -vx;
             }
 
-            if ( x_ball > right ) { /* 超过右侧 */
+            if ( x_ball > right ) {
                 x_ball = 2 * l - 2 * r - x_ball, vx = -vx;
             }
 
-            if ( ( y_ball > bottom ) && ( ( x_ball > x_rect - w_rect / 2 - r / 1.41 ) && ( x_ball < x_rect + w_rect / 2 + r / 1.41 ) ) ) { /* 小球触到矩阵块 */
+            if ( ( y_ball > bottom ) && ( ( x_ball > x_rect - w_rect / 2 - r / 1.41 ) && ( x_ball < x_rect + w_rect / 2 + r / 1.41 ) ) ) {
                 y_ball = 2 * ( bottom + r ) - 2 * r - y_ball, vy = -vy;
                 score_num++;
 
@@ -56,7 +53,7 @@ void simulate ( void ) {
                 } else if ( ( score_num >= 10 ) && ( score_num <= 99 ) ) {
                     LCD_ShowNum ( 50, 450, score_num, 2, 16 );
                 }
-            } else if ( ( y_ball > bottom ) && ( ( x_ball < x_rect - w_rect / 2  - r / 1.41 ) || ( x_ball > x_rect + w_rect / 2 + - r / 1.41 ) ) ) { /* 小球未触到矩阵块 */
+            } else if ( ( y_ball > bottom ) && ( ( x_ball < x_rect - w_rect / 2  - r / 1.41 ) || ( x_ball > x_rect + w_rect / 2 + - r / 1.41 ) ) ) {
                 LCD_Clear ( GREEN );
                 POINT_COLOR = RED;
                 LCD_ShowString ( 0, 450, 50, 50, 16, "score:" );
@@ -67,7 +64,7 @@ void simulate ( void ) {
                 }
             }
 
-            if ( y_ball < top ) { /* 超过顶部 */
+            if ( y_ball < top ) {
                 y_ball = 2 * r - y_ball, vy = -vy;
             }
         }
@@ -76,21 +73,20 @@ void simulate ( void ) {
         delay_ms ( 10 );
         LCD_Fill ( x_ball - r, y_ball - r, x_ball + r, y_ball + r, WHITE );
 
-        /*-------------------------------------------------------------------------------------------------------------*/
-        if ( move_left == 1 ) { /* 矩阵块向左移动 */
-            move_left = 0; /* 对移动标志进行复位 */
+        if ( move_left == 1 ) {
+            move_left = 0;
 
-            if ( x_rect > w_rect / 2 ) { /* 如果未移动到左边缘 */
+            if ( x_rect > w_rect / 2 ) {
                 LCD_Fill ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2, WHITE );
                 x_rect -= move_step;
                 LCD_Fill ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2, RED );
             }
         }
 
-        if ( move_right == 1 ) { /* 矩阵块向右移动 */
-            move_right = 0; /* 对移动标志进行复位 */
+        if ( move_right == 1 ) {
+            move_right = 0;
 
-            if ( x_rect < l - w_rect / 2 ) { /* 如果未移动到右边缘 */
+            if ( x_rect < l - w_rect / 2 ) {
                 LCD_Fill ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2, WHITE );
                 x_rect += move_step;
                 LCD_Fill ( x_rect - w_rect / 2,  y_rect - h_rect / 2, x_rect + w_rect / 2, y_rect + h_rect / 2, RED );
@@ -98,28 +94,28 @@ void simulate ( void ) {
         }
     }
 
-#if 0
-    vy = sin ( 45 * 3.14 / 180 ) * 5; /* x方向上速度分量 */
-    vx = cos ( 45 * 3.14 / 180 ) * 5; /* y方向上速度分量 */
+#if 1
+    vy = sin ( 45 * 3.14 / 180 ) * 5;
+    vx = cos ( 45 * 3.14 / 180 ) * 5;
 
     while ( 1 ) {
-        x_ball += vx; /* 增加x方向的分量 */
-        y_ball -= vy; /* 增加y方向的分量 */
+        x_ball += vx;
+        y_ball -= vy;
 
         while ( ( x_ball < left ) || ( x_ball > right ) || ( y_ball > bottom ) || ( y_ball < top ) ) {
-            if ( x_ball < left ) { /* 超过左侧 */
+            if ( x_ball < left ) {
                 x_ball = 2 * r - x_ball, vx = -vx;
             }
 
-            if ( x_ball > right ) { /* 超过右侧 */
+            if ( x_ball > right ) {
                 x_ball = 2 * l - 2 * r - x_ball, vx = -vx;
             }
 
-            if ( y_ball > bottom ) { /* 超过底部 */
+            if ( y_ball > bottom ) {
                 y_ball = 2 * w - 2 * r - y_ball, vy = -vy;
             }
 
-            if ( y_ball < top ) { /* 超过顶部 */
+            if ( y_ball < top ) {
                 y_ball = 2 * r - y_ball, vy = -vy;
             }
         }
@@ -128,21 +124,17 @@ void simulate ( void ) {
         delay_ms ( 10 );
         LCD_Fill ( x_ball - r, y_ball - r, x_ball + r, y_ball + r, WHITE );
     }
-
 #endif
 }
 
-
-/*-------------------------------------------------------*/
-
 #define DISABLE_JTAG do \
     { \
-        RCC->APB2ENR |= 0x00000001; /* 开启afio时钟 */ \
-        AFIO->MAPR = ( 0x00FFFFFF & AFIO->MAPR ) | 0x04000000; /* 关闭JTAG */ \
+        RCC->APB2ENR |= 0x00000001; \
+        AFIO->MAPR = ( 0x00FFFFFF & AFIO->MAPR ) | 0x04000000; \
     }while(0);
 
 int main ( void ) {
-    DISABLE_JTAG; /* 关闭JTAG功能 */
+    DISABLE_JTAG;
     SystemInit();
     delay_init ( 72 );
     NVIC_Configuration();
@@ -156,4 +148,3 @@ int main ( void ) {
 
     while ( 1 );
 }
-
